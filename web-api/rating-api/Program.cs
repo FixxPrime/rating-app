84852b673f2b629669643d8c4224ed43363cc665
FixxPrime <fixprimeoff@mail.ru>
2024-04-27 00:12:51 +0500

    add-auto-migration

--- web-api/rating-api/Program.cs
+++ web-api/rating-api/Program.cs
@@ -41,6 +41,12 @@ builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
